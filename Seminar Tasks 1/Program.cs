c49242f30f52f1af0b41ad408ab4b74742003abe
ZloRiK <r.zrk@yandex.ru>
2022-11-14 21:42:16 +0500

    решена первая задача

---
+++ Seminar Tasks 1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write ("Введите первое число ");
+int a = int.Parse (Console.ReadLine()!);
+Console.Write ("Введите второе число ");
+int b = int.Parse (Console.ReadLine()!);
+
+int max = a;
+
+if (a>b)
+{
+    Console.WriteLine ($"max равен {max}");
+}
+else
+{
+    max = b;
+    Console.WriteLine ($"max равен {max}");
+}
