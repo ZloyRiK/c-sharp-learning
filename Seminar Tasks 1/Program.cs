23c752254a5a88e1e97bb47f3f14083e0b333f65
ZloRiK <r.zrk@yandex.ru>
2022-11-14 22:01:01 +0500

    решена третья задача

--- Seminar Tasks 1/Program.cs
+++ Seminar Tasks 1/Program.cs
@@ -19,17 +19,31 @@
 
 // Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
-Console.Write ("Введите первое число ");
+// Console.Write ("Введите первое число ");
+// int a = int.Parse (Console.ReadLine()!);
+// Console.Write ("Введите второе число ");
+// int b = int.Parse (Console.ReadLine()!);
+// Console.Write ("Введите третье число ");
+// int c = int.Parse (Console.ReadLine()!);
+
+// int max = a;
+
+// if (a > max) max = a;
+// if (b > max) max = b;
+// if (c > max) max = c;
+
+// Console.WriteLine ($"Максимальное число {max}");
+
+//  Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.Write ("Введите число для проверки четности ");
 int a = int.Parse (Console.ReadLine()!);
-Console.Write ("Введите второе число ");
-int b = int.Parse (Console.ReadLine()!);
-Console.Write ("Введите третье число ");
-int c = int.Parse (Console.ReadLine()!);
 
-int max = a;
-
-if (a > max) max = a;
-if (b > max) max = b;
-if (c > max) max = c;
-
-Console.WriteLine ($"Максимальное число {max}");
+if ((a%2)==0)
+{
+    Console.WriteLine ($"{a} - четное число");
+}
+else
+{
+    Console.WriteLine ($"{a} - нечетное число");
+}
