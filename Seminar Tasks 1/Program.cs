9273c00a53851c7a2c3794d5758594a4acd6b276
ZloRiK <r.zrk@yandex.ru>
2022-11-14 21:55:57 +0500

    решена вторая задача

--- Seminar Tasks 1/Program.cs
+++ Seminar Tasks 1/Program.cs
@@ -1,16 +1,35 @@
-﻿Console.Write ("Введите первое число ");
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+
+// Console.Write ("Введите первое число ");
+// int a = int.Parse (Console.ReadLine()!);
+// Console.Write ("Введите второе число ");
+// int b = int.Parse (Console.ReadLine()!);
+
+// int max = a;
+
+// if (a>b)
+// {
+//     Console.WriteLine ($"max равен {max}");
+// }
+// else
+// {
+//     max = b;
+//     Console.WriteLine ($"max равен {max}");
+// }
+
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write ("Введите первое число ");
 int a = int.Parse (Console.ReadLine()!);
 Console.Write ("Введите второе число ");
 int b = int.Parse (Console.ReadLine()!);
+Console.Write ("Введите третье число ");
+int c = int.Parse (Console.ReadLine()!);
 
 int max = a;
 
-if (a>b)
-{
-    Console.WriteLine ($"max равен {max}");
-}
-else
-{
-    max = b;
-    Console.WriteLine ($"max равен {max}");
-}
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.WriteLine ($"Максимальное число {max}");
