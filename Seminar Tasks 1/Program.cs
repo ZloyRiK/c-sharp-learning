8db5e603c78c5cce7a27998c3f17efc92a4911cc
ZloRiK <r.zrk@yandex.ru>
2022-11-14 22:17:16 +0500

    решена четвертая задача

--- Seminar Tasks 1/Program.cs
+++ Seminar Tasks 1/Program.cs
@@ -36,14 +36,27 @@
 
 //  Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 
-Console.Write ("Введите число для проверки четности ");
-int a = int.Parse (Console.ReadLine()!);
+// Console.Write ("Введите число для проверки четности ");
+// int a = int.Parse (Console.ReadLine()!);
 
-if ((a%2)==0)
+// if ((a%2)==0)
+// {
+//     Console.WriteLine ($"{a} - четное число");
+// }
+// else
+// {
+//     Console.WriteLine ($"{a} - нечетное число");
+// }
+
+// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+
+Console.Write ("Введите число ");
+int N = int.Parse (Console.ReadLine()!);
+
+int index = 1;
+
+while (index <= N)
 {
-    Console.WriteLine ($"{a} - четное число");
-}
-else
-{
-    Console.WriteLine ($"{a} - нечетное число");
+    if ((index % 2)==0) Console.Write ($"{index} ");
+    index++;
 }
