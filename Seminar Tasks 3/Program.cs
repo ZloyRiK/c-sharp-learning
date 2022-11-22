c9685516599411fb5ae791c15baea571b793b24a
ZloRiK <r.zrk@yandex.ru>
2022-11-22 20:14:22 +0500

    исправил ошибку в задаче и добавил проверку на пятизначность

--- Seminar Tasks 3/Program.cs
+++ Seminar Tasks 3/Program.cs
@@ -1,21 +1,27 @@
 ﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом(первое число равно последнему, второе равно предпоследнему)
 
-// Console.Write("Введите пятизначное число ");
-// int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите пятизначное число ");
+int number1 = int.Parse(Console.ReadLine());
 
-// string num1 = Convert.ToString(number1);
+string num1 = Convert.ToString(number1);
 
-// int length = num1.Length;
+int length = num1.Length;
 
-// if (num1[0] == num1[4] && num1[1] == num1[3])
-// {
-//     Console.WriteLine($"{number1} - это полиндром");
-// }
-// else
-// {
-//    Console.WriteLine($"{ number1} не является полиндромом");
-// }
-
+if (length==5)
+{
+    if (num1[0] == num1[4] && num1[1] == num1[3])
+    {
+        Console.WriteLine($"{number1} - это полиндром");
+    }
+    else
+    {
+        Console.WriteLine($"{ number1} не является полиндромом");
+    }   
+}
+else
+{
+    Console.WriteLine ("Вы ввели не пятизначное число");
+}
 
 //Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 
@@ -37,15 +43,15 @@
 
 //Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
-Console.Write("Введите колличество степеней для тройки ");
-int N = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите колличество степеней для тройки ");
+// int N = int.Parse(Console.ReadLine()!);
 
-if (N<0)
-{
-    Console.Write ("Число преобразовано в положительное");
-    N = -N;
-}
-for (int i=1; i<=N; i++)
-{
-    Console.WriteLine ($"{Math.Pow(3,i)} ");
-}
+// if (N<0)
+// {
+//     Console.Write ("Число преобразовано в положительное");
+//     N = -N;
+// }
+// for (int i=1; i<=N; i++)
+// {
+//     Console.WriteLine ($"{Math.Pow(i,3)} ");
+// }
