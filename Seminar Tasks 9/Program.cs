ba7b24ab8c574a6376374923f01647559c2fc51e
ZloRiK <r.zrk@yandex.ru>
2022-12-15 08:55:28 +0500

    решены задачи к 9 семинару

---
+++ Seminar Tasks 9/Program.cs
@@ -0,0 +1,73 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+
+
+// Console.Write("Введите число N: ");
+// int n = int.Parse(Console.ReadLine()!);
+// int count = 1;
+// PrintNumber(n, count);
+
+// void PrintNumber(int n, int count)
+// {
+//     if (count > n) return;
+//     PrintNumber(n, count + 1);
+//     Console.Write($"{count} ");
+// }
+
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// Console.Write("Введите число M: ");
+// int m = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите число N: ");
+// int n = int.Parse(Console.ReadLine()!);
+// int temp = m;
+
+// if (m > n) 
+// {
+//   m = n; 
+//   n = temp;
+// }
+
+// PrintSumm(m, n, temp=0);
+
+// void PrintSumm(int m, int n, int summ)
+// {
+//   summ = summ + n;
+//   if (n <= m)
+//   {
+//     Console.Write($"Сумма натуральных чисел указанного промежутка равна {summ} ");
+//     return;
+//   }
+//   PrintSumm(m, n - 1, summ);
+// }
+
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+Console.Write("Введите число M: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine()!);
+
+int functionAkkerman = Akkerman(m, n);
+
+Console.Write($"Функция Аккермана = {functionAkkerman} ");
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else
+    {
+        if (n == 0) 
+        {
+            return Akkerman(m - 1, 1);
+        }
+        else
+        {
+            return Akkerman(m - 1, Akkerman(m, n - 1));
+        }
+    }
+}
